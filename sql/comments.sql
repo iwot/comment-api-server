933d5d02238534b1ec3981a00e50d36d21a9cf68
iwot <iwmt@outlook.com>
2019-10-04 16:33:12 +0900

    取得クエリにコメント日時を追加

--- sql/comments.sql
+++ sql/comments.sql
@@ -21,7 +21,7 @@ values (:page_id, :name, :mail, :comment, :ip)
 
 -- :name comments-by-page :? :*
 -- :doc Comments
-select id, page_id, name, comment
+select id, page_id, name, comment, created_at
   from comments
  where page_id = :page_id and approved = 1
  order by id desc
